699e8542ea8166b219ef7fd5a35121d86e25450e
zloster <more@edno.moe>
2017-04-24 17:27:32 +0300

    Add 2 versions of servlet-postgres-raw

--- frameworks/Java/servlet/setup_v42.sh
+++ frameworks/Java/servlet/setup_v42.sh
@@ -4,7 +4,7 @@ sed -i 's|localhost|'"${DBHOST}"'|g' src/main/webapp/WEB-INF/resin-web.xml
 
 fw_depends java resin maven postgresql
 
-mvn -P v9 clean compile war:war
+mvn -P v42 clean compile war:war
 rm -rf $RESIN_HOME/webapps/*
 cp target/servlet.war $RESIN_HOME/webapps/
 resinctl start
